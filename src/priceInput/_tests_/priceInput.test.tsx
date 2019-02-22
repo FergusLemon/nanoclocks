577d65ce0bb22f5d8ff8d6ad7448e851fc6cde38
Fergus Lemon <fergus.lemon@gmail.com>
2019-02-22 10:17:10 +0000

    Update to use the getElement helper function and bring in Enzyme adapter - issue opened to find reason why setupTest.ts file not being found by Jest.

--- src/priceInput/_tests_/priceInput.test.tsx
+++ src/priceInput/_tests_/priceInput.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import '../../setupTests';
 import { shallow } from 'enzyme';
 import PriceInput from '../components/priceInput';
+import getElement from '../../common/utils/getElement';
+
+const setup = (input = {}) => (
+  {
+    title: input.id || 'id',
+    value: input.value || 'value',
+  }
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PriceInput />, div);
+});
+
+describe("PriceInput", () => {
+  it('passes the id prop to a label', () => {
+    const testEnv = setup();
+    const wrapper = shallow(<PriceInput {...testEnv} />);
+    expect(getElement(wrapper)('label')('price-input-label').length).toBe(1);
+  });
 });
