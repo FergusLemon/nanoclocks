046e1ecd14cc119500484417e2ce8893f3e4bd4a
Fergus Lemon <fergus.lemon@gmail.com>
2019-02-22 18:42:10 +0000

    Test to check that min, max and step values were being passed down on props and assigned to the relevant attributes on the component.

--- src/priceInput/_tests_/priceInput.test.tsx
+++ src/priceInput/_tests_/priceInput.test.tsx
@@ -5,34 +5,61 @@ import { shallow } from 'enzyme';
 import PriceInput from '../components/priceInput';
 import getElement from '../../common/utils/getElement';
 
-const setup = (input = {}) => (
+const DEFAULT = undefined,
+      SEVEN = 7,
+      MIN = "0",
+      MAX = "40",
+      STEP = "0.05",
+      setup = (input = {}) => (
   {
     id: input.id || 'id',
-    value: input.value || 'value',
+    value: input.value || DEFAULT,
+    min: input.min || MIN,
+    max: input.max || MAX,
+    step: input.step || STEP,
     handleBlur: input.handleBlur || jest.fn(),
     handleKeyDown: input.handleKeyDown || jest.fn()
   }
 );
-const DEFAULT = undefined;
-const SEVEN = 7;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PriceInput />, div);
 });
 
-describe("PriceInput", () => {
-  it('passes the id prop to a label', () => {
+describe('PriceInput', () => {
+  it('passes the "id" prop to a label', () => {
     const testEnv = setup();
     const wrapper = shallow(<PriceInput {...testEnv} />);
     expect(getElement(wrapper)('label')('price-input-label').length).toBe(1);
   });
 
-  it('passes the value prop to the value of the text input', () => {
+  it('passes the "value" prop to the value of the text input', () => {
     const testEnv = setup({ value: SEVEN });
     const wrapper = shallow(<PriceInput {...testEnv} />);
     expect(getElement(wrapper)('input')('price-input-field').props().value)
       .toBe(SEVEN);
+  });
+
+  it('passes the "min" prop to the value of the min attribute', () => {
+    const testEnv = setup({ min: MIN });
+    const wrapper = shallow(<PriceInput {...testEnv} />);
+    expect(getElement(wrapper)('input')('price-input-field').props().min)
+      .toBe(MIN);
+  });
+
+  it('passes the "max" prop to the value of the max attribute', () => {
+    const testEnv = setup({ max: MAX });
+    const wrapper = shallow(<PriceInput {...testEnv} />);
+    expect(getElement(wrapper)('input')('price-input-field').props().max)
+      .toBe(MAX);
+  });
+
+  it('passes the "step" prop to the value of the step attribute', () => {
+    const testEnv = setup({ step: STEP });
+    const wrapper = shallow(<PriceInput {...testEnv} />);
+    expect(getElement(wrapper)('input')('price-input-field').props().step)
+      .toBe(STEP);
   });
 
     it('should call the handleBlur callback on props with the current value when the input changes', () => {
--- src/priceInput/components/priceInput.tsx
+++ src/priceInput/components/priceInput.tsx
@@ -4,16 +4,22 @@ import '../styles/priceInput.css';
 type Props = {
   id: string
   value: number | undefined
+  min: string
+  max: string
+  step: string
   handleBlur(event: FocusEvent<HTMLInputElement>): void
   handleKeyDown(event: KeyboardEvent<HTMLInputElement>): void
 }
 
-const PriceInput: SFC<Props> = ({ id, value, handleBlur, handleKeyDown, children }) => (
+const PriceInput: SFC<Props> = ({ id, value, min, max, step, handleBlur, handleKeyDown, children }) => (
   <div className="price-input-field-container">
     <label htmlFor={id} className="price-input-label">Enter a price</label>
-    <input className="price-input-field" type="number" min="0"
+    <input className="price-input-field" type="number"
       id={id}
       value={value}
+      min={min}
+      max={max}
+      step={step}
       onBlur={handleBlur}
       onKeyDown={ev => (ev.key === 'Enter' || ev.key === '-') && handleKeyDown(ev)}
     />
