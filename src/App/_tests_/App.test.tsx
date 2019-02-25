397895ed88b4ed22358b89a52f542092dea4bd66
Fergus Lemon <fergus.lemon@gmail.com>
2019-02-25 18:03:00 +0000

    Improve RegExp used to validate user input.

--- src/App/_tests_/App.test.tsx
+++ src/App/_tests_/App.test.tsx
@@ -9,6 +9,10 @@ const DEFAULT_VALUE = "",
       MINUS = "-",
       MIN = 5,
       MAX = 40,
+      ONE_DECIMAL = 5.1,
+      TWO_DECIMAL = 5.15,
+      THREE_DECIMAL = 5.155,
+      ZERO_NUMBER = "01",
       INVALID = "Invalid",
       setup = (input = {}) => (
   {
@@ -50,6 +54,40 @@ describe("App", () => {
       expect(wrapper.state().value).toBe(validAmount);
     });
 
+    it('sets the value of state when passed a valid two digit number where the first digit is less than the minimum from the PriceInput component', () => {
+      let validAmount = (MIN + 30).toString();
+      event.currentTarget.value = validAmount;
+      wrapper.find("PriceInput").props().handleChange(event);
+      expect(wrapper.state().value).toBe(validAmount);
+    });
+
+    it('sets the value of state when passed a valid number with one decimal place from the PriceInput component', () => {
+      let validAmountOneDecimal = ONE_DECIMAL.toString();
+      event.currentTarget.value = validAmountOneDecimal;
+      wrapper.find("PriceInput").props().handleChange(event);
+      expect(wrapper.state().value).toBe(validAmountOneDecimal);
+    });
+
+    it('sets the value of state when passed a valid number with two decimal places from the PriceInput component', () => {
+      let validAmountTwoDecimal = TWO_DECIMAL.toString();
+      event.currentTarget.value = validAmountTwoDecimal;
+      wrapper.find("PriceInput").props().handleChange(event);
+      expect(wrapper.state().value).toBe(validAmountTwoDecimal);
+    });
+
+    it('does not set the value of state when passed a number with more than two decimal places from the PriceInput component', () => {
+      let validAmountThreeDecimal = THREE_DECIMAL.toString();
+      event.currentTarget.value = validAmountThreeDecimal;
+      wrapper.find("PriceInput").props().handleChange(event);
+      expect(wrapper.state().value).toBe(DEFAULT_VALUE);
+    });
+
+    it('does not set the value of state when passed a zero followed by another number from the PriceInput component', () => {
+      event.currentTarget.value = ZERO_NUMBER;
+      wrapper.find("PriceInput").props().handleChange(event);
+      expect(wrapper.state().value).toBe(DEFAULT_VALUE);
+    });
+
     it('does not set the value of state when passed a minus sign from the PriceInput component', () => {
       event.currentTarget.value = MINUS;
       wrapper.find("PriceInput").props().handleChange(event);
@@ -62,7 +100,7 @@ describe("App", () => {
       expect(wrapper.state().value).toBe(DEFAULT_VALUE);
     });
 
-    it('does not set the value of state when passed a number lower than the minimum from the PriceInput component', () => {
+    xit('does not set the value of state when passed a number lower than the minimum from the PriceInput component', () => {
       let underLimit = MIN - 1;
       event.currentTarget.value = underLimit.toString();
       wrapper.find("PriceInput").props().handleChange(event);
