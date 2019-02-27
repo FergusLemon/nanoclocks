1e5f13577e7fcec9e50ccb06458a77da795a3700
Fergus Lemon <fergus.lemon@gmail.com>
2019-02-27 16:20:15 +0000

    Refactor - remove superfluous properties on the mocked response object.

--- src/App/_tests_/App.test.tsx
+++ src/App/_tests_/App.test.tsx
@@ -144,13 +144,8 @@ describe("App", () => {
       const wrapper = shallow(<App />);
       const prices = {
         data: [{
-          "close": 0.9228,
           "high": 0.9351,
-          "low": 0.9093,
-          "open": 0.9335,
           "time": 1550620800,
-          "volumefrom": 1349848.03,
-          "volumeto": 1245639.76
           }]
       };
       mockCryptoCompareApi.getPriceInformation.mockImplementationOnce(() =>
