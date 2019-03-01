a000dafb75dd3a39005eee749ec2e2c75a349e73
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-01 11:16:01 +0000

    Create common utility for a binary search function that will eturn the index of the nearst higher price if the price the user entered does not exist in the priceHistory on state in the App component.

---
+++ src/common/utils/nearestElementBinarySearch.ts
@@ -0,0 +1,19 @@
+function binarySearch (list: Array<string>, item: string) {
+  let low: number = 0;
+  let high: number = (list.length - 1);
+
+  while (low <= high) {
+    let mid: number = Math.floor((low + high) / 2);
+    let guess: string = list[mid];
+    if (guess === item) {
+      return mid;
+    } else if (guess > item) {
+      high = mid - 1;
+    } else {
+        low = mid + 1;
+    }
+  }
+  return low;
+};
+
+export default binarySearch;
