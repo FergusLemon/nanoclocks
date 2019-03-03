007dadd7a68f177b6f0a65ab80a4047a36df37de
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-03 18:28:56 +0000

    Test that calculateDifference is called on mounting and implement function.

--- src/clock/_tests_/Clock.test.tsx
+++ src/clock/_tests_/Clock.test.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import * as React from 'react';
 import { shallow } from 'enzyme';
 import '../../setupTests';
 import getElement from '../../common/utils/getElement';
 import Clock from '../components/Clock';
 
+const DEFAULT_TIME = 1550000000;
+const setup = (input = {}) => (
+  {
+    lastTime: input.lastTime || DEFAULT_TIME,
+  }
+);
+
 describe('Clock', () => {
   it('renders without crashing', () => {
     shallow(<Clock />);
+  });
+
+  it('calls the calculateDifference function on mounting', () => {
+    const testEnv = setup({});
+    const wrapper = shallow(<Clock {...testEnv} />);
+    const spy = jest.spyOn(wrapper.instance(), 'calculateDifference');
+
+    expect(spy).toHaveBeenCalledOnce;
+
+    spy.mockRestore();
   });
 });
--- src/clock/components/Clock.tsx
+++ src/clock/components/Clock.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import * as React from 'react';
 
-const initialState = {};
+const initialState = {
+  timeSince: 0,
+  currentTime: parseInt((Date.now() / 1000).toFixed()),
+};
 
 type State = Readonly<typeof initialState>;
+type Props = {
+  lastTime: number
+};
 
-class Clock extends React.Component<object, State> {
+class Clock extends React.Component<Props, State> {
   readonly state: State = initialState;
 
-  render() {}
+  calculateDifference = () => {
+    return this.state.currentTime - this.props.lastTime;
+  };
+
+  componentDidMount() {
+    let difference: number = this.calculateDifference();
+    this.setState({
+      timeSince: difference,
+    });
+  };
+
+  render() {
+    const { lastTime } = this.props;
+    return (
+      <div className="clock">{this.state.timeSince}</div>
+    )
+  }
 };
 
 export default Clock;
