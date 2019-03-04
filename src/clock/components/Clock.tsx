4bfffdbc2becbfa3c2659cd39794cfdec813dce7
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-04 17:07:29 +0000

    Refactor to DRY out code and make display cleaner by adding a leading zero if the timeframe is less than 10.

--- src/clock/components/Clock.tsx
+++ src/clock/components/Clock.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import moment from 'moment';
 
 const defaultValue: number = 0,
+      doubleDigits: number = 10,
       maxMonths: number = 11,
       maxDays: number = 30,
       maxHours: number = 23,
       maxMinutes: number = 59,
       maxSeconds: number = 59;
-const initialState = {
+
+const initialState: any = {
   years: defaultValue,
   months: defaultValue,
   days: defaultValue,
@@ -32,7 +34,6 @@ class Clock extends React.Component<Props, State> {
   };
 
   calculateDifference = () => {
-    //if (this.props.lastTime === 0) return;
     const now = this.state.currentTime;
     const then = this.props.lastTime * 1000;
     const duration: any = moment.duration(moment(now).diff(moment(then)));
@@ -47,61 +48,68 @@ class Clock extends React.Component<Props, State> {
     });
   };
 
-  incrementDifference = () => {
-    if (this.state.seconds < maxSeconds) {
+  incrementDifference = (): void => {
+    if (this.shouldIncrementSeconds()) {
       this.setState({
         seconds: this.state.seconds + 1
       });
-    } else if (this.state.seconds === maxSeconds && this.state.minutes < maxMinutes) {
-      this.setState({
-        minutes: this.state.minutes + 1,
-        seconds: defaultValue
-      });
-    } else if (this.state.minutes === maxMinutes && this.state.hours < maxHours) {
-      this.setState({
-        hours: this.state.hours + 1,
-        minutes: defaultValue,
-        seconds: defaultValue
-      });
-    } else if (this.state.hours === maxHours && this.state.days < maxDays) {
-      this.setState({
-        days: this.state.days + 1,
-        hours: defaultValue,
-        minutes: defaultValue,
-        seconds: defaultValue
-      });
-    } else if (this.state.days === maxDays && this.state.months < maxMonths) {
-      this.setState({
-        months: this.state.months + 1,
-        days: defaultValue,
-        hours: defaultValue,
-        minutes: defaultValue,
-        seconds: defaultValue
-      });
+    } else if (this.shouldIncrementMinutes()) {
+      this.updateState("minutes", ["seconds"]);
+    } else if (this.shouldIncrementHours()) {
+      this.updateState("hours", ["seconds", "minutes"]);
+    } else if (this.shouldIncrementDays()) {
+      this.updateState("days", ["seconds", "minutes", "hours"]);
+    } else if (this.shouldIncrementMonths()) {
+      this.updateState("months", ["seconds", "minutes", "hours", "days"]);
     } else {
+      this.updateState("years", ["seconds", "minutes", "hours", "days", "months"]);
+    };
+  };
+
+  shouldIncrementSeconds = (): boolean => {
+    return this.state.seconds < maxSeconds;
+  };
+
+  shouldIncrementMinutes = (): boolean => {
+    return this.state.seconds === maxSeconds && this.state.minutes < maxMinutes;
+  };
+
+  shouldIncrementHours = (): boolean => {
+    return this.state.minutes === maxMinutes && this.state.hours < maxHours;
+  };
+
+  shouldIncrementDays = (): boolean => {
+    return this.state.hours === maxHours && this.state.days < maxDays;
+  };
+
+  shouldIncrementMonths = (): boolean => {
+    return this.state.days === maxDays && this.state.months < maxMonths;
+  };
+
+  updateState = (toIncrement: string, toReset: Array<string>): void => {
+    this.setState({
+      [toIncrement]: this.state[toIncrement] + 1
+    });
+    for (let property of toReset) {
       this.setState({
-        years: this.state.years + 1,
-        months: defaultValue,
-        days: defaultValue,
-        hours: defaultValue,
-        minutes: defaultValue,
-        seconds: defaultValue
+        [property]: defaultValue
       });
     };
   };
 
   render() {
     const { lastTime, children } = this.props;
+    let { years, months, days, hours, minutes, seconds } = this.state;
     return (
       <div className="clock-container">
         { lastTime !== 0 &&
           <div className="clock">
-            {this.state.years}-
-            {this.state.months}-
-            {this.state.days}-
-            {this.state.hours}:
-            {this.state.minutes}:
-            {this.state.seconds}
+            {years < doubleDigits ? 0 : ''}{years}-
+            {months < doubleDigits ? 0 : ''}{months}-
+            {days < doubleDigits ? 0 : ''}{days}-
+            {hours < doubleDigits ? 0 : ''}{hours}:
+            {minutes < doubleDigits ? 0 : ''}{minutes}:
+            {seconds < doubleDigits ? 0 : ''}{seconds}
           </div>
         }
     </div>
