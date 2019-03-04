0094ff2c019d67fcc5ccc414b57d550f0f4db3fc
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-04 15:21:26 +0000

    Test for and implement functionality that increments a clock each second.

--- src/clock/_tests_/Clock.test.tsx
+++ src/clock/_tests_/Clock.test.tsx
@@ -4,10 +4,18 @@ import '../../setupTests';
 import getElement from '../../common/utils/getElement';
 import Clock from '../components/Clock';
 
-const DEFAULT_TIME = 1550000000;
+const defaultTime = 1550000000,
+      timer = 1000,
+      defaultValue = 0,
+      maxSeconds = 59,
+      maxMinutes = maxSeconds,
+      maxHours = 23,
+      maxDays = 30,
+      maxMonths = 11,
+      withinRange = 5;
 const setup = (input = {}) => (
   {
-    lastTime: input.lastTime || DEFAULT_TIME,
+    lastTime: input.lastTime || defaultTime,
   }
 );
 
@@ -34,10 +42,148 @@ describe('Clock', () => {
     spy.mockRestore();
   });
 
-  it('sets the value of timeSince on state when mounting', () => {
+  it(`sets the value of year, month, day, hour, minute and second on state when
+    mounting`, () => {
     const testEnv = setup({});
     const wrapper = shallow(<Clock {...testEnv} />);
 
-    expect(wrapper.state().timeSince).not.toEqual(0);
+    expect(wrapper.state().seconds).not.toEqual(defaultValue);
+  });
+
+  it('tells the browser to call the setInterval function whilst mounted',
+    () => {
+    jest.useFakeTimers();
+    const testEnv = setup({});
+    const wrapper = shallow(<Clock {...testEnv} />);
+
+    expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), timer);
+  });
+
+  it('calls the setInterval function with incrementDifference', () => {
+    jest.useFakeTimers();
+    const testEnv = setup({});
+    const wrapper = shallow(<Clock {...testEnv} />);
+    const incrementDifference = wrapper.instance().incrementDifference;
+
+    expect(setInterval).toHaveBeenLastCalledWith(incrementDifference, timer);
+  });
+
+  describe('updating state using the timer', () => {
+    let testEnv, wrapper;
+    beforeEach(() => {
+      jest.useFakeTimers();
+      testEnv = setup({});
+      wrapper = shallow(<Clock {...testEnv} />);
+    });
+
+    describe(`when the value of seconds on state is less than the maximum`,
+      () => {
+      it('increments the value on state by one', () => {
+        wrapper.setState({ seconds: defaultValue });
+
+        jest.advanceTimersByTime(timer);
+
+        expect(wrapper.state().seconds).toEqual(defaultValue + 1);
+      });
+    });
+
+    describe(`when the value of seconds on state is at the maximum but minutes
+      is not`, () => {
+      it(`increments the value of minutes on state by one and resets the value
+          of lower timeframes to zero`, () => {
+        wrapper.setState({
+          minutes: withinRange,
+          seconds: maxSeconds
+        });
+
+        jest.advanceTimersByTime(timer);
+
+        expect(wrapper.state().minutes).toEqual(withinRange + 1);
+        expect(wrapper.state().seconds).toEqual(defaultValue);
+      });
+    });
+
+    describe(`when the value of minutes on state is at the maximum but hours is
+      not`, () => {
+        it(`increments the value of hours on state by one and resets the value of
+          lower timeframes to zero`, () => {
+          wrapper.setState({
+            hours: withinRange,
+            minutes: maxMinutes,
+            seconds: maxSeconds
+          });
+
+          jest.advanceTimersByTime(timer);
+
+          expect(wrapper.state().hours).toEqual(withinRange + 1);
+          expect(wrapper.state().minutes).toEqual(defaultValue);
+          expect(wrapper.state().seconds).toEqual(defaultValue);
+      });
+    });
+
+    describe(`when the value of hours on state is at the maximum but days is
+      not`, () => {
+        it(`increments the value of days on state by one and resets the value of
+          lower timframes to zero`, () => {
+          wrapper.setState({
+            days: withinRange,
+            hours: maxHours,
+            minutes: maxMinutes,
+            seconds: maxSeconds
+          });
+
+          jest.advanceTimersByTime(timer);
+
+          expect(wrapper.state().days).toEqual(withinRange + 1);
+          expect(wrapper.state().hours).toEqual(defaultValue);
+          expect(wrapper.state().minutes).toEqual(defaultValue);
+          expect(wrapper.state().seconds).toEqual(defaultValue);
+      });
+    });
+
+    describe(`when the value of days on state is at the maximum but months is
+      not`, () => {
+        it(`increments the value of months on state by one and resets the value
+          of lower timframes to zero`, () => {
+          wrapper.setState({
+            months: withinRange,
+            days: maxDays,
+            hours: maxHours,
+            minutes: maxMinutes,
+            seconds: maxSeconds
+          });
+
+          jest.advanceTimersByTime(timer);
+
+          expect(wrapper.state().months).toEqual(withinRange + 1);
+          expect(wrapper.state().days).toEqual(defaultValue);
+          expect(wrapper.state().hours).toEqual(defaultValue);
+          expect(wrapper.state().minutes).toEqual(defaultValue);
+          expect(wrapper.state().seconds).toEqual(defaultValue);
+      });
+    });
+
+    describe('when the value of months on state is at the maximum', () => {
+      it(`increments the value of years on state by one and resets the value of
+        lower timframes to zero`, () => {
+        wrapper.setState({
+          years: defaultValue,
+          months: maxMonths,
+          days: maxDays,
+          hours: maxHours,
+          minutes: maxMinutes,
+          seconds: maxSeconds
+        });
+
+        jest.advanceTimersByTime(timer);
+
+        expect(wrapper.state().years).toEqual(defaultValue + 1);
+        expect(wrapper.state().months).toEqual(defaultValue);
+        expect(wrapper.state().days).toEqual(defaultValue);
+        expect(wrapper.state().hours).toEqual(defaultValue);
+        expect(wrapper.state().minutes).toEqual(defaultValue);
+        expect(wrapper.state().seconds).toEqual(defaultValue);
+      });
+    });
   });
 });
--- src/clock/components/Clock.tsx
+++ src/clock/components/Clock.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
+import moment from 'moment';
 
+const defaultValue: number = 0,
+      maxMonths: number = 11,
+      maxDays: number = 30,
+      maxHours: number = 23,
+      maxMinutes: number = 59,
+      maxSeconds: number = 59;
 const initialState = {
-  timeSince: 0,
-  currentTime: parseInt((Date.now() / 1000).toFixed()),
+  years: defaultValue,
+  months: defaultValue,
+  days: defaultValue,
+  hours: defaultValue,
+  minutes: defaultValue,
+  seconds: defaultValue,
+  currentTime: Date.now(),
 };
 
 type State = Readonly<typeof initialState>;
@@ -13,15 +25,69 @@ type Props = {
 class Clock extends React.Component<Props, State> {
   readonly state: State = initialState;
 
-  calculateDifference = () => {
-    return this.state.currentTime - this.props.lastTime;
-  };
 
   componentDidMount() {
-    let difference: number = this.calculateDifference();
+    this.calculateDifference();
+    setInterval(this.incrementDifference, 1000);
+  };
+
+  calculateDifference = () => {
+    //if (this.props.lastTime === 0) return;
+    const now = this.state.currentTime;
+    const then = this.props.lastTime * 1000;
+    const duration: any = moment.duration(moment(now).diff(moment(then)));
+    const durationHash = duration._data;
     this.setState({
-      timeSince: difference,
+      years: durationHash['years'],
+      months: durationHash['months'],
+      days: durationHash['days'],
+      hours: durationHash['hours'],
+      minutes: durationHash['minutes'],
+      seconds: durationHash['seconds'],
     });
+  };
+
+  incrementDifference = () => {
+    if (this.state.seconds < maxSeconds) {
+      this.setState({
+        seconds: this.state.seconds + 1
+      });
+    } else if (this.state.seconds === maxSeconds && this.state.minutes < maxMinutes) {
+      this.setState({
+        minutes: this.state.minutes + 1,
+        seconds: defaultValue
+      });
+    } else if (this.state.minutes === maxMinutes && this.state.hours < maxHours) {
+      this.setState({
+        hours: this.state.hours + 1,
+        minutes: defaultValue,
+        seconds: defaultValue
+      });
+    } else if (this.state.hours === maxHours && this.state.days < maxDays) {
+      this.setState({
+        days: this.state.days + 1,
+        hours: defaultValue,
+        minutes: defaultValue,
+        seconds: defaultValue
+      });
+    } else if (this.state.days === maxDays && this.state.months < maxMonths) {
+      this.setState({
+        months: this.state.months + 1,
+        days: defaultValue,
+        hours: defaultValue,
+        minutes: defaultValue,
+        seconds: defaultValue
+      });
+    } else {
+      this.setState({
+        years: this.state.years + 1,
+        months: defaultValue,
+        days: defaultValue,
+        hours: defaultValue,
+        minutes: defaultValue,
+        seconds: defaultValue
+      });
+    };
   };
 
   render() {
@@ -29,7 +95,14 @@ class Clock extends React.Component<Props, State> {
     return (
       <div className="clock-container">
         { lastTime !== 0 &&
-          <div className="clock">{this.state.timeSince}</div>
+          <div className="clock">
+            {this.state.years}-
+            {this.state.months}-
+            {this.state.days}-
+            {this.state.hours}:
+            {this.state.minutes}:
+            {this.state.seconds}
+          </div>
         }
     </div>
     )
