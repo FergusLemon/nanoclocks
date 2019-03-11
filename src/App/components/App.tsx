6983e44d15a293f5471fe9c3b5c5f551b4b73fa0
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-11 17:53:38 +0000

    Move comments about the regex used into the helper file.

--- src/App/components/App.tsx
+++ src/App/components/App.tsx
@@ -166,15 +166,3 @@ class App extends React.Component<object, State> {
 }
 
 export default App;
-
-    //RegExp used for valdiating numbers entered as strings by the user, see
-    //issue #2 in the Github repo for explanation outline of the issue of
-    //controlled components and input fields of type number in HTML.
-    //Non-capturing groups were used.
-    //First RegExp group matches an empty string.
-    //Second group matches a single 0.
-    //Third group matches a single 0 with a decimal point
-    //and then between zero and two digits that range between 0-9.
-    //Fourth group matches numbers in the range 1-99.
-    //Fifth group matches numbers in the same range as the fourth group but
-    //includes decimal numbers up to two decimal places.
--- src/common/utils/validNumberRegex.ts
+++ src/common/utils/validNumberRegex.ts
@@ -1,3 +1,14 @@
 const regex: RegExp = /^$|^(?:0)$|^(?:0\.[0-9]{0,2})$|^(?:[1-9]{1}[0-9]{0,1})$|^(?:[1-9]{1}[0-9]{0,1}\.[0-9]{0,2})$/
 
 export default regex;
+//RegExp used for valdiating numbers entered as strings by the user, see
+//issue #2 in the Github repo for explanation outline of the issue of
+//controlled components and input fields of type number in HTML.
+//Non-capturing groups were used.
+//First RegExp group matches an empty string.
+//Second group matches a single 0.
+//Third group matches a single 0 with a decimal point
+//and then between zero and two digits that range between 0-9.
+//Fourth group matches numbers in the range 1-99.
+//Fifth group matches numbers in the same range as the fourth group but
+//includes decimal numbers up to two decimal places.
