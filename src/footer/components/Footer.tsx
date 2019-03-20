bba68e14afea6739ff2d17e068772fde7a019b30
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-20 13:16:31 +0000

    add class names to social media links

--- src/footer/components/Footer.tsx
+++ src/footer/components/Footer.tsx
@@ -8,19 +8,19 @@ const Footer: SFC = ({ children }) => (
       Join The Conversation
     </p>
     <div className="social-media-container">
-      <a href="https://www.reddit.com/r/nanocurrency">
+      <a href="https://www.reddit.com/r/nanocurrency" className="reddit">
         <FontAwesomeIcon icon={['fab', 'reddit']} className="icon" />
       </a>
-      <a href="https://twitter.com/nano?lang=en">
+      <a href="https://twitter.com/nano?lang=en" className="twitter">
         <FontAwesomeIcon icon={['fab', 'twitter']} className="icon" />
       </a>
-      <a href="https://discordapp.com/invite/JphbBas">
+      <a href="https://discordapp.com/invite/JphbBas" className="discord">
         <FontAwesomeIcon icon={['fab', 'discord']} className="icon" />
       </a>
-      <a href="https://medium.com/nanocurrency">
+      <a href="https://medium.com/nanocurrency" className="medium">
         <FontAwesomeIcon icon={['fab', 'medium']} className="icon" />
       </a>
-      <a href="https://github.com/nanocurrency">
+      <a href="https://github.com/nanocurrency" className="github">
         <FontAwesomeIcon icon={['fab', 'github']} className="icon" />
       </a>
     </div>
