d1968a3327a61954d67fadccbd74961ac4277856
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-20 12:34:41 +0000

    add social media icons and links

--- src/footer/components/Footer.tsx
+++ src/footer/components/Footer.tsx
@@ -1,11 +1,29 @@
 import React, { SFC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/Footer.css';
 
 const Footer: SFC = ({ children }) => (
   <footer className="footer-container">
-    <p className="App-footer-test">
-      Footer
+    <p className="footer-message">
+      Join The Conversation
     </p>
+    <div className="social-media-container">
+      <a href="https://www.reddit.com/r/nanocurrency">
+        <FontAwesomeIcon icon={['fab', 'reddit']} className="icon" />
+      </a>
+      <a href="https://twitter.com/nano?lang=en">
+        <FontAwesomeIcon icon={['fab', 'twitter']} className="icon" />
+      </a>
+      <a href="https://discordapp.com/invite/JphbBas">
+        <FontAwesomeIcon icon={['fab', 'discord']} className="icon" />
+      </a>
+      <a href="https://medium.com/nanocurrency">
+        <FontAwesomeIcon icon={['fab', 'medium']} className="icon" />
+      </a>
+      <a href="https://github.com/nanocurrency">
+        <FontAwesomeIcon icon={['fab', 'github']} className="icon" />
+      </a>
+    </div>
   </footer>
 )
 
