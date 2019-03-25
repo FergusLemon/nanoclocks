c43e4b1958adfc30f30c0c71bf06a57090674d93
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-25 13:27:58 +0000

    test that the current price shown changes at set intervals and update test cases for the use of the 'display' className.

--- src/priceDisplay/_tests_/PriceDisplay.test.tsx
+++ src/priceDisplay/_tests_/PriceDisplay.test.tsx
@@ -5,6 +5,7 @@ import PriceDisplay from '../components/PriceDisplay'
 import getElement from '../../common/utils/getElement'
 
 const DEFAULT = '';
+const timer = 6000;
 const setup = (input = {}) => (
   {
     children: {
@@ -22,20 +23,37 @@ describe('PriceInput', () => {
     shallow(<PriceDisplay {...testEnv} />);
   });
 
-  it('has displays default values for props when none are passed down', () => {
+  it('displays default values for props when none are passed down', () => {
     const testEnv = setup();
     const wrapper = shallow(<PriceDisplay {...testEnv} />);
 
-    expect(getElement(wrapper)('li')('btc').text()).not.toContain(/[0-9]/);
-    expect(getElement(wrapper)('li')('usd').text()).not.toContain(/[0-9]/);
+    expect(getElement(wrapper)('li')('display').text()).not.toContain(/[0-9]/);
   });
 
-  it('has displays values passed in on props', () => {
+  it('displays values passed in on props', () => {
+    const testEnv = setup({children:{BTC: 0.00025}});
+    const {BTC} = testEnv.children;
+    const wrapper = shallow(<PriceDisplay {...testEnv} />);
+
+    expect(getElement(wrapper)('li')('display').text()).toContain(BTC);
+  });
+
+  it('calls the setInterval function with updatePriceToDisplay', () => {
+    jest.useFakeTimers();
+    const testEnv = setup({});
+    const wrapper = shallow(<PriceDisplay {...testEnv} />);
+    const updatePriceToDisplay = wrapper.instance().updatePriceToDisplay;
+
+    expect(setInterval).toHaveBeenLastCalledWith(updatePriceToDisplay, timer);
+  });
+
+  it('changes the price displayed at a specified interval', () => {
+    jest.useFakeTimers();
     const testEnv = setup({children:{BTC: 0.00025, USD: 1.00}});
     const {BTC, USD} = testEnv.children;
     const wrapper = shallow(<PriceDisplay {...testEnv} />);
 
-    expect(getElement(wrapper)('li')('btc').text()).toContain(BTC);
-    expect(getElement(wrapper)('li')('usd').text()).toContain(USD);
+    jest.advanceTimersByTime(timer);
+    expect(getElement(wrapper)('li')('display').text()).toContain(USD);
   });
 });
