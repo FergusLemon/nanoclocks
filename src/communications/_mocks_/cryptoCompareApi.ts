2e168fec056218baeb36de79652981c67ec37f9e
Fergus Lemon <fergus.lemon@gmail.com>
2019-03-31 16:37:15 +0100

    remove unneccessary return keyword

--- src/communications/_mocks_/cryptoCompareApi.ts
+++ src/communications/_mocks_/cryptoCompareApi.ts
@@ -12,9 +12,9 @@ export const priceHistorySpec: object = {
   volumefrom: 1349848.03,
 };
 
-export const getCurrentPriceMock = jest.fn().mockImplementation(() => return new Promise((resolve, reject) => resolve([currentPriceSpec])));
+export const getCurrentPriceMock = jest.fn().mockImplementation(() => new Promise((resolve, reject) => resolve([currentPriceSpec])));
 
-export const getPriceInformationMock = jest.fn().mockImplementation(() => return new Promise((resolve, reject) => resolve([priceHistorySpec])));
+export const getPriceInformationMock = jest.fn().mockImplementation(() => new Promise((resolve, reject) => resolve([priceHistorySpec])));
 
 const mock = jest.fn().mockImplementation(() => {
   return {
